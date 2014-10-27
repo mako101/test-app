7ba28227261a0acda4739e032c8ca4ce541235a1
viktor <viktor@ZyXEL0.(none)>
2014-10-27 22:35:32 +0000

    Added node app

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+ 
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('You aren\'t supposed to be here!!');
+});
+ 
+app.get('/hello', function (req, res) {
+  res.send('hello world');
+});
+ 
+app.listen(process.env.PORT || 5000);
+ 
+module.exports = app;
