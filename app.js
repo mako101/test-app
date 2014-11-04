a6bb093e79cb6f1e00d1ad480765726859e3ee16
Viktor <viktor@home>
2014-11-04 22:16:57 +0000

    Changed the response + test

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello jenkins');
 });
  
 app.get('/hello', function (req, res) {
-  res.send('hello jenkins');
+  res.send('hello world');
 });
  
 app.listen(process.env.PORT || 5000);
