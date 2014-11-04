a3094fb1b7d1d78a8df77f3e533dce7091fc2eb8
viktor <viktor@ZyXEL0.(none)>
2014-11-04 22:11:55 +0000

    Changed the response + test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('respond with hello jenkins', function(done) {
-    request(app).get('/').expect('hello jenkins', done);
+    request(app).get('/').expect('hello world', done);
   });
 });
 
